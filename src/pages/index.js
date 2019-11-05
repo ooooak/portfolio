95f91521d2d5cb1d90d8501ca6ff11195bd9cc10
ooooak <akshay.deep0@gmail.com>
2019-11-05 15:39:35 +0530

    small update

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,11 @@ const IndexPage = () => (
       </div>
 
       <div className="description">
-        <p>I'm a Full Stack Web Developer from Punjab, India with 7+ years of experience. I have a strong background in PHP/Laravel/Wordpress, Node/Express and React/Nextjs. Interested FP, Clojure, Elixir/OTP and Distributed System.</p>
+        <p>I'm a Full Stack Developer from India with 7+ years of experience. I have a strong background in PHP/Laravel, Node/Express and React/Nextjs. Interested FP, Clojure, Elixir/OTP and Software Architecture.</p>
         <br/>
       </div>
       <div className="social-media">
-        <a href="mailto:akshay.deep0@gmail.com?Subject=Hi Akshay!!" target="_blank"> Email </a>
+        <a href="mailto:akshay.deep0@gmail.com?Subject=Hi Akshay!!" target="_blank">Email</a>
         <span> / </span>
         <a title="Twitter" href="https://twitter.com/ooooak" target="_blank">Twitter</a>
         <span> / </span>
